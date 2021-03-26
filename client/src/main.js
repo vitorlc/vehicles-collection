ed59022361a88d6f0d83c28da0a213ea20edce96
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-26 07:05:53 -0300

    Add SweetAlert

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import lang from 'element-ui/lib/locale/lang/pt-br'
+import Swal from 'sweetalert2'
 
+Vue.prototype.$swal = Swal
 Vue.use(ElementUI, { locale: lang })
 Vue.config.productionTip = false
 
