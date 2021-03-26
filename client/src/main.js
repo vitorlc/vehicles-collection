c4a9b57721a7d5d19b1a82bf48334007ab3b54f3
vitorlc <vitor_cirqueira1@hotmail.com>
2021-03-25 21:25:28 -0300

    Configure lang pt-br on element ui

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import lang from 'element-ui/lib/locale/lang/pt-br'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale: lang })
 Vue.config.productionTip = false
 
 new Vue({
